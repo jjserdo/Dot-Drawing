d0b8845ee39407abbf8837cbfd57966cac78edc3
jjserdo <justineserdoncillo@gmail.com>
2023-04-09 17:01:06 -0500

    Functionality Updates
    put necessary foundation for new functionalities

--- README.md
+++ README.md
@@ -22,15 +22,24 @@ This is one of my dream projects to create. I've always liked those dotted noteb
     - Choose file is working tho
 
 
-# Things to do still
-- [ ] Snap properly at the center
+# Things to do still [/] done [c] foundation
+- [/] Snap properly at the center
 - [ ] Ability to cut off lines for any intersections
-- [ ] keyboard shortcuts to change colors
-- [ ] keyboard shortcuts to change line thickness
+- [c] keyboard shortcuts to change colors
+- [c] keyboard shortcuts to change line thickness
 - [ ] Main menu to show the different types of grids
-- [ ] Add css using bootstrap?
+- [c] Add css using bootstrap?
 - [ ] Make it touch compatible if using a phone
 - [ ] Save functionality
     - [ ] saves rows and columns into a 2d array
     - [ ] creates an (N,2,2) array which is a list of all of the two points which has coordinates (x,y) 
-- [ ] line creation doesn't even work anymore lmao
+    - [ ] saves to your local computer and you can choose where to solve it 
+    - [ ] add comments on the bottom or top what version this is
+- [/] line creation doesn't even work anymore lmao
+- [ ] make dots smaller and make it gray
+- [ ] add a button to change between light and dark mode
+- [ ] add button so I can remove the dots
+- [?] maybe use a slider from the domestika projects
+- [/] center the title
+- [ ] how do I see what the lines are storing
+- [ ] if changing the slider then store the lines and then just remove the right and bottom rows or smth
--- your-script.js
+++ your-script.js
@@ -1,22 +1,32 @@
+// Create canvas
 let canvas = new fabric.Canvas('canvas', {
   width: window.innerWidth,
   height: window.innerHeight
 });
 canvas.selection = false;
+canvas.perPixelTargetFind = true;
 
+// Initialize slider elements
 const sliderRows = document.getElementById('rows');
 const sliderCols = document.getElementById('cols');
 const sliderRowsValue = document.getElementById('rows-value');
 const sliderColsValue = document.getElementById('cols-value');
-const DOT_COUNT = 25; // Default number of dots when the page is loaded
-let DOT_RADIUS = 15;
 
+// Set default values for dot and grid properties
+const DOT_COUNT = 25; 
+let DOT_RADIUS = 10;
 let NUM_ROWS = sliderRows.value;
 let NUM_COLS = sliderCols.value;
-
 sliderRowsValue.textContent = NUM_ROWS;
 sliderColsValue.textContent = NUM_COLS;
 
+// Initialize lines and dots arrays
+let lines = [];
+let line = null;
+let startDot = null;
+let dots = [];
+
+// Add slider event listeners
 sliderRows.addEventListener('input', event => {
   NUM_ROWS = event.target.value;
   sliderRowsValue.textContent = NUM_ROWS;
@@ -29,13 +39,14 @@ sliderCols.addEventListener('input', event => {
   updateDots();
 });
 
+// Update dots on the canvas
 function updateDots() {
   canvas.clear();
   dots = [];
   lines = [];
 
   // Calculate the radius of the dots based on the number of rows and columns
-  DOT_RADIUS = Math.min(canvas.width / NUM_COLS, canvas.height / NUM_ROWS) / 6;
+  DOT_RADIUS = Math.min(canvas.width / NUM_COLS, canvas.height / NUM_ROWS) / 8;
 
   for (let row = 0; row < NUM_ROWS; row++) {
     for (let col = 0; col < NUM_COLS; col++) {
@@ -54,11 +65,6 @@ function updateDots() {
 }
 
 updateDots();
-
-canvas.perPixelTargetFind = true;
-
-let line = null;
-let startDot = null;
 
 canvas.on('mouse:down', event => {
   let target = event.target;
@@ -94,6 +100,11 @@ canvas.on('mouse:up', event => {
       let center = target.getCenterPoint();
       line.set({ x2: center.x, y2: center.y });
       line.endDot = target; // Set the 'endDot' property
+
+      let startDotRowCol = getRowColFromCoordinates(line.startDot.left, line.startDot.top);
+      let endDotRowCol = getRowColFromCoordinates(line.endDot.left, line.endDot.top);
+      lines.push({ start: startDotRowCol, end: endDotRowCol });
+
     } else {
       // Released outside a dot
       canvas.remove(line);
@@ -112,9 +123,6 @@ document.addEventListener('keydown', event => {
       startDot = null;
     }
   }
-});
-
-document.addEventListener('keydown', event => {
   if (event.keyCode === 32) {
     // Space bar pressed
     if (lines.length > 0) {
@@ -124,17 +132,6 @@ document.addEventListener('keydown', event => {
   }
 });
 
-// Add the remove button to the page
-let removeButton = document.createElement('div');
-removeButton.style.position = 'absolute';
-removeButton.style.bottom = '20px';
-removeButton.style.right = '20px';
-removeButton.style.borderRadius = '10px';
-removeButton.style.backgroundColor = 'lightgray';
-removeButton.style.padding = '10px';
-removeButton.style.cursor = 'pointer';
-removeButton.innerHTML = 'x';
-document.body.appendChild(removeButton);
 
 // Add a click event listener to the remove button
 removeButton.addEventListener('click', function() {
@@ -144,28 +141,39 @@ removeButton.addEventListener('click', function() {
   });
 });
 
+// Save button event listener
 document.getElementById('save-button').addEventListener('click', () => {
+  // Prepare lines data to save only relevant properties
+  const preparedLines = lines.map(line => {
+    const startDotRowCol = getRowColFromCoordinates(line.startDot.left, line.startDot.top);
+    const endDotRowCol = getRowColFromCoordinates(line.endDot.left, line.endDot.top);
+    return { start: startDotRowCol, end: endDotRowCol };
+  });
+
   // Construct the data to be saved
   const saveData = {
     rows: NUM_ROWS,
     cols: NUM_COLS,
-    lines: []
+    lines: lines
   };
-  for (let i = 0; i < lines.length; i++) {
-    let line = lines[i];
-    let startDotIndex = dots.indexOf(line.startDot);
-    let endDotIndex = dots.indexOf(line.endDot);
-    saveData.lines.push([startDotIndex, endDotIndex]);
-  }
 
   // Convert the data to a JSON string
   const saveDataString = JSON.stringify(saveData);
 
+    // Create a Blob from the JSON string
+  const saveDataBlob = new Blob([saveDataString], { type: 'text/plain;charset=utf-8' });
+
+  // Create a URL for the Blob
+  const saveDataURL = URL.createObjectURL(saveDataBlob);
+
   // Create a download link with the data and click it to open the file explorer
   const downloadLink = document.createElement('a');
-  downloadLink.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(saveDataString);
+  downloadLink.href = saveDataURL;
   downloadLink.download = 'interactive-canvas-save.txt';
   downloadLink.click();
+
+  // Clean up the URL object to avoid memory leaks
+  URL.revokeObjectURL(saveDataURL);
 });
 
 // Load button functionality
@@ -202,3 +210,90 @@ fileInput.addEventListener('change', event => {
   };
   reader.readAsText(file);
 });
+
+// Utility functions for converting between row/col and x/y coordinates
+function getCoordinatesFromRowCol(row, col) {
+  return {
+    x: (col + 0.25) * (canvas.width / NUM_COLS),
+    y: (row + 0.25) * (canvas.height / NUM_ROWS)
+  };
+}
+
+function getRowColFromCoordinates(x, y) {
+  return {
+    row: Math.round((y * NUM_ROWS) / canvas.height - 0.25),
+    col: Math.round((x * NUM_COLS) / canvas.width - 0.25)
+  };
+}
+
+function drawLinesFromRowCols() {
+  lines.forEach(line => {
+    const startCoords = getCoordinatesFromRowCol(line.start.row, line.start.col);
+    const endCoords = getCoordinatesFromRowCol(line.end.row, line.end.col);
+
+    const drawnLine = new fabric.Line([startCoords.x + DOT_RADIUS, startCoords.y + DOT_RADIUS, endCoords.x + DOT_RADIUS, endCoords.y + DOT_RADIUS], {
+      stroke: 'black',
+      strokeWidth: 3,
+      originX: 'center',
+      originY: 'center',
+    });
+    canvas.add(drawnLine);
+  });
+}
+
+/* These are buttons and stuff*/
+
+// Add the remove button to the page
+let removeButton = document.createElement('div');
+removeButton.style.position = 'absolute';
+removeButton.style.bottom = '20px';
+removeButton.style.right = '20px';
+removeButton.style.borderRadius = '10px';
+removeButton.style.backgroundColor = 'lightgray';
+removeButton.style.padding = '10px';
+removeButton.style.cursor = 'pointer';
+removeButton.innerHTML = 'x';
+document.body.appendChild(removeButton);
+
+let toggleDotsButton = document.createElement('div');
+toggleDotsButton.style.position = 'absolute';
+toggleDotsButton.style.bottom = '60px';
+toggleDotsButton.style.right = '20px';
+toggleDotsButton.style.borderRadius = '10px';
+toggleDotsButton.style.backgroundColor = 'lightgray';
+toggleDotsButton.style.padding = '10px';
+toggleDotsButton.style.cursor = 'pointer';
+toggleDotsButton.innerHTML = 'Toggle Dots';
+document.body.appendChild(toggleDotsButton);
+
+toggleDotsButton.addEventListener('click', function() {
+  dots.forEach(dot => {
+    dot.visible = !dot.visible;
+  });
+  canvas.renderAll();
+});
+
+/* 
+  Make it touch-compatible: You can use Fabric.js touch event listeners (e.g., 'touch:gesture', 'touch:drag', 'touch:orientation', and 'touch:shake') to handle touch interactions on mobile devices. Refer to the Fabric.js documentation for more details: http://fabricjs.com/events
+  Undo/redo functionality: Allow users to undo or redo their actions by maintaining a history of drawn lines and removed lines.
+
+Color selection: Add a color picker so users can choose the color of the lines they draw.
+
+Line thickness: Add a slider to allow users to adjust the thickness of the lines they draw.
+
+Export to image: Allow users to export their canvas as an image (e.g., PNG or JPEG) for easy sharing.
+
+Touch support: Add touch event listeners to make the canvas work smoothly on mobile devices and tablets.
+
+Zoom and pan: Implement zooming and panning functionality to help users navigate large canvases more easily.
+
+Snap to grid: Make lines snap to a grid for more precise drawing.
+
+Text tool: Allow users to add text labels to the canvas.
+
+Import from image: Allow users to import an image and use it as a background for their canvas.
+
+Layer management: Implement layer functionality so users can organize their lines, dots, and other elements in separate layers.
+These features can be implemented using Fabric.js's built-in functionality, as well as with additional JavaScript libraries if needed.
+*/
+
